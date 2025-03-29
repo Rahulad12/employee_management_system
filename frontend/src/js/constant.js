ca40cc80a51b408c9c2abd0c3ca0746e14f5a4ee
Rahul Adhikari <rahul077@academiacollege.edu.np>
2025-03-29 21:07:33 +0545

    added backend url

--- frontend/src/js/constant.js
+++ frontend/src/js/constant.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:5000/api";
+export const BASE_URL = "https://employeebackend-7bu0.onrender.com/api";
 export const AUTH_URL = `${BASE_URL}/auth`;
 export const EMP_URL = `${BASE_URL}/employee`;
 export const LEAVE_URL = `${BASE_URL}/leave/`;
